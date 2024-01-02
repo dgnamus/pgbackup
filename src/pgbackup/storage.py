1976d761539b68872cd1475a1af98c32d447366b
David Gnamus <david.gnamus@protonmail.com>
2024-01-02 14:49:24 +0000

    implemented s3 interaction

--- src/pgbackup/storage.py
+++ src/pgbackup/storage.py
@@ -2,3 +2,6 @@ def local(infile, outfile):
     outfile.write(infile.read())
     outfile.close()
     infile.close()
+
+def remote(client, infile, bucket, name):
+    client.upload_fileobj(infile, bucket, name)
