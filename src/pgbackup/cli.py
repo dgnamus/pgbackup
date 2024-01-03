276f6d7582518f8e0152b192e9087d5ce6fc887e
David Gnamus <david.gnamus@protonmail.com>
2024-01-03 15:14:00 +0000

    Added required python version

--- src/pgbackup/cli.py
+++ src/pgbackup/cli.py
@@ -11,9 +11,10 @@ def create_parser():
     parser = ArgumentParser(description="Backup postgresql locally or to aws s3")
 
     parser.add_argument("url", help="URL of the database to backup")
-    parser.add_argument("--driver",
+    parser.add_argument("--driver", "-d",
             help="how and where to store backup",
             nargs=2,
+            metavar=("DRIVER", "DESTINATION"),
             action=DriverAction,
             required=True)
 
@@ -21,13 +22,16 @@ def create_parser():
 
 def main():
     import boto3
+    import time
     from pgbackup import pgdump, storage
 
     args = create_parser().parse_args()
     dump = pgdump.dump(args.url)
     if args.driver == 'remote':
         client = boto3.client('s3')
-        storage.remote(client, dump.stdout, args.destination, 'example.sql')
+        timestamp = time.strftime("%Y-%m-%dT-%H:%M", time.localtime())
+        file_name = pgdump.dump_file_name(args.url, timestamp)
+        storage.remote(client, dump.stdout, args.destination, file_name)
     else:
         outfile = open(args.destination, 'wb')
         storage.local(dump.stdout, outfile)
--- src/pgbackup/pgdump.py
+++ src/pgbackup/pgdump.py
@@ -8,4 +8,12 @@ def dump(url):
         print(f"Error: {err}")
         sys.exit(1)
 
-
+def dump_file_name(url, timestamp=None):
+    # Below, we split the url, and we will get only the database name, because the name comes after the last slash
+    db_name = url.split('/')[-1]
+    # And below here we filter out if there are any queries attached to the end
+    db_name = db_name.split('?')[0]
+    if timestamp:
+        return f"{db_name}-{timestamp}.sql"
+    else:
+        return f"{db_name}.sql"
